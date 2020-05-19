ccb1ae8d2933b00fba6c2d1caf3b1e8457163e26
Andyeyo <oaramos414@gmail.com>
2020-05-19 11:58:35 -0500

    [ADD] QReader App

---
+++ app/src/main/java/com/example/qrscanner/MainActivity.java
@@ -0,0 +1,154 @@
+package com.example.qrscanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import github.nisrulz.qreader.QRDataListener;
+import github.nisrulz.qreader.QREader;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView txt_result;
+    private SurfaceView surfaceView;
+    private QREader qrEader;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final View parentLayout = findViewById(android.R.id.content);
+        // Request permission
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.CAMERA)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        setupCamera();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                        Snackbar snackbar = Snackbar
+                                .make(parentLayout, "Please enable permission", Snackbar.LENGTH_LONG);
+                        snackbar.show();
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+
+                    }
+                }).check();
+    }
+
+    private void setupCamera(){
+        txt_result = findViewById(R.id.code_info);
+        final ToggleButton btn_on_off = findViewById(R.id.btn_enable_disable);
+        btn_on_off.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(qrEader.isCameraRunning()){
+                    btn_on_off.setChecked(false);
+                    qrEader.stop();
+                }
+                else {
+                    btn_on_off.setChecked(true);
+                    qrEader.start();
+                }
+            }
+        });
+
+        surfaceView = findViewById(R.id.camera_view);
+        setupQReader();
+    }
+
+    private void setupQReader(){
+        qrEader = new QREader.Builder(this, surfaceView, new QRDataListener() {
+            @Override
+            public void onDetected(final String data) {
+                txt_result.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        txt_result.setText(data);
+                    }
+                });
+            }
+        }).facing(QREader.BACK_CAM)
+                .enableAutofocus(true)
+                .height(surfaceView.getHeight())
+                .width(surfaceView.getWidth())
+                .build();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        final View parentLayout = findViewById(android.R.id.content);
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.CAMERA)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        if(qrEader != null)
+                            qrEader.initAndStart(surfaceView);
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                        Snackbar snackbar = Snackbar
+                                .make(parentLayout, "Please enable permission", Snackbar.LENGTH_LONG);
+                        snackbar.show();
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+
+                    }
+                }).check();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        final View parentLayout = findViewById(android.R.id.content);
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.CAMERA)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        if(qrEader != null)
+                            qrEader.releaseAndCleanup();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                        Snackbar snackbar = Snackbar
+                                .make(parentLayout, "Please enable permission", Snackbar.LENGTH_LONG);
+                        snackbar.show();
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+
+                    }
+                }).check();
+    }
+}
